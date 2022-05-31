aebe51907059272e0a3e75d4e41775fa4cac46be
Hiago <hiago.chagas.14@gmail.com>
2022-05-31 14:38:00 -0300

    added example for presentation

--- src/arvoreAVL/Main.java
+++ src/arvoreAVL/Main.java
@@ -2,33 +2,31 @@ package arvoreAVL;
 
 public class Main {
     public static void main(String[] args) {
-        BinaryNode<Integer> node1 = new BinaryNode<>(1);
-        BinaryNode<Integer> node2 = new BinaryNode<>(2);
-        BinaryNode<Integer> node3 = new BinaryNode<>(3);
-        BinaryNode<Integer> node4 = new BinaryNode<>(4);
-        BinaryNode<Integer> node5 = new BinaryNode<>(5);
-        BinaryNode<Integer> node6 = new BinaryNode<>(6);
-        BinaryNode<Integer> node7 = new BinaryNode<>(7);
-        BinaryTree<Integer> tree = new BinaryTree<>(node1);
-//        tree.addNode(node2, node1, BinaryPosition.left);
-//        tree.addNode(node3, node1, BinaryPosition.right);
-//
-//        tree.addNode(node4, node2, BinaryPosition.left);
-//        tree.addNode(node5, node2, BinaryPosition.right);
-//
-//        tree.addNode(node6, node3, BinaryPosition.left);
-//        tree.addNode(node7, node3, BinaryPosition.right);
-//        
-        tree.addNode(node2);
-        tree.addNode(node3);
+        BinaryNode<Integer> node50 = new BinaryNode<>(50);
+        BinaryNode<Integer> node17 = new BinaryNode<>(17);
+        BinaryNode<Integer> node76 = new BinaryNode<>(76);
+        BinaryNode<Integer> node9 = new BinaryNode<>(9);
+        BinaryNode<Integer> node23 = new BinaryNode<>(23);
+        BinaryNode<Integer> node54 = new BinaryNode<>(54);
+        BinaryNode<Integer> node14 = new BinaryNode<>(14);
+        BinaryNode<Integer> node19 = new BinaryNode<>(19);
+        BinaryNode<Integer> node72 = new BinaryNode<>(72);
+        BinaryNode<Integer> node12 = new BinaryNode<>(12);
+        BinaryNode<Integer> node67 = new BinaryNode<>(67);
+        BinaryTree<Integer> tree = new BinaryTree<>(node50);
+        tree.addNode(node17, node50, BinaryPosition.left);
+        tree.addNode(node76, node50, BinaryPosition.right);
+        tree.addNode(node9, node17, BinaryPosition.left);
+        tree.addNode(node23, node17, BinaryPosition.right);
+        tree.addNode(node54, node76, BinaryPosition.left);
+        tree.addNode(node14, node9, BinaryPosition.right);
+        tree.addNode(node19, node23, BinaryPosition.left);
+        tree.addNode(node72, node54, BinaryPosition.right);
+        tree.addNode(node12, node14, BinaryPosition.left);
+        tree.addNode(node67, node72, BinaryPosition.left);
 
-        tree.addNode(node4);
-        tree.addNode(node5);
-
-        tree.addNode(node6);
-        tree.addNode(node7);
-
-        BinaryNode<Integer> foundNode = tree.searchNode(7);
-        System.out.println(foundNode.getParent().getDescription());
+        tree.print();
+        tree.avlBalance();
+        tree.print();
     }
 }
